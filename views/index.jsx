a571d3754d5cda80725ca5b78e49c9037aef28a1
Yvette <libra1014tyb@163.com>
2016-08-04 22:42:52 +0800

    task4

--- views/index.jsx
+++ views/index.jsx
@@ -20,12 +20,14 @@ class TodoList extends React.Component {
                     <tbody>
                     <Todo title="Shopping">Milk</Todo>
                     <Todo title="Hair cut">13:00</Todo>
+                    <Todo title="Learn React">15:00</Todo>
                     </tbody>
                 </table>
             </div>
         );
     }
 }
+
 
 class Todo extends React.Component {
     render(){
@@ -37,6 +39,10 @@ class Todo extends React.Component {
         );
     }
 }
+
+Todo.propTypes = {
+    title: React.PropTypes.string.isRequired
+};
 
 class TodoForm extends React.Component {
     render(){
