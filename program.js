5a6b9e84bf9570fc99dd952210791834bc13b6dc
Yvette <libra1014tyb@163.com>
2016-08-05 22:48:43 +0800

    task8

--- program.js
+++ program.js
@@ -1,30 +1,78 @@
 var express = require('express');
 var app = express();
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var DOM = React.DOM;
+var body = DOM.body;
+var div = DOM.div;
+var script = DOM.script;
 
-app.set('port', (process.argv[2] || 3000));
-app.set('view engine', 'jsx');
-app.set('views', __dirname + '/views');
-app.engine('jsx', require('express-react-views').createEngine({transformViews: false}));
+var browserify = require('browserify');
+var babelify = require("babelify");
 
 require('babel/register')({
     ignore: false
 });
 
-// write below
-var data = [
-    {
-        title: "Shopping",
-        detail: process.argv[3]
-    },
-    {
-        title:"Hair cut",
-        detail:process.argv[4]
-    }
-];
+var TodoBox = require('./views/index.jsx');
+
+
+// app.set('port', (process.argv[2] || 3000));
+// app.set('view engine', 'jsx');
+// app.set('views', __dirname + '/views');
+// app.engine('jsx', require('express-react-views').createEngine({transformViews: false}));
+
+// require('babel/register')({
+//     ignore: false
+// });
+
+// var data = [
+//     {
+//         title: "Shopping",
+//         detail: process.argv[3]
+//     },
+//     {
+//         title:"Hair cut",
+//         detail:process.argv[4]
+//     }
+// ];
+
+// app.use('/', function (req, res) {
+//     res.render('index', {data: data});
+// });
+//
+// app.listen(app.get('port'), function () {
+// });
+
+app.use('/bundle.js', function (req, res) {
+    res.setHeader('content-type', 'application/javascript');
+
+    browserify({ debug: true })
+        .transform(babelify.configure({
+            presets: ["react", "es2015"],
+            compact: false
+        }))
+        .require("./app.js", { entry: true })
+        .bundle()
+        .pipe(res);
+});
 
 app.use('/', function (req, res) {
-    res.render('index', {data: data});
+    var initialData = JSON.stringify(data);
+    var markup = ReactDOMServer.renderToString(React.createElement(TodoBox, {data: data}));
+
+    res.setHeader('Content-Type', 'text/html');
+
+    var html = ReactDOMServer.renderToStaticMarkup(body(null,
+        div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}),
+        script({
+            id: 'initial-data',
+            type: 'text/plain',
+            'data-json': initialData
+        }),
+        script({src: '/bundle.js'})
+    ));
+
+    res.end(html);
 });
 
-app.listen(app.get('port'), function () {
-});
